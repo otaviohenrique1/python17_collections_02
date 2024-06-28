f590543ba3850498f8d16e471bc6a6bf11ce9e8a
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-28 10:11:53 -0300

    commit 4

--- 01_conjuntos.py
+++ 01_conjuntos.py
@@ -1,2 +1,12 @@
-usuarios_data_science = [15, 23, 43, 56]
-usuarios_machine_learning = [13, 23, 56, 42]
+usuarios_data_science = {15, 23, 43, 56}
+usuarios_machine_learning = {13, 23, 56, 42}
+assistiram = usuarios_data_science | usuarios_machine_learning
+print(assistiram)
+# set => conjunto => Exemplo: {1, 2, 3}
+# print(set(assistiram))
+assistiram2 = usuarios_data_science & usuarios_machine_learning
+print(assistiram2)
+assistiram3 = usuarios_data_science - usuarios_machine_learning
+print(assistiram3)
+assistiram4 = usuarios_data_science ^ usuarios_machine_learning
+print(assistiram4)
---
+++ 02_operacoes.py
@@ -0,0 +1,10 @@
+usuarios = {1,5,76,34,52,13,17}
+print(usuarios)
+print(len(usuarios))
+usuarios.add(765)
+print(usuarios)
+# frozenset => conjunto imutavel
+print(frozenset(usuarios))
+meu_texto = "Bem vindo meu nome é Guilherme eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro"
+palavras = set(meu_texto.split())
+print(palavras)
